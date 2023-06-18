9bf1bce70238857bed3a1c7d4a25aa69965aaab4
JITHIN KS <jithinkaranath@gmail.com>
2023-06-18 20:36:58 +0530

    changes pushed

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import { Button } from '@chakra-ui/react';
+import { Button, FormController, Grid, GridItem, Box } from '@chakra-ui/react';
 
 const JComponents = {
-    Button
+    Button, FormController, Grid, GridItem, Box
 }
 
 module.exports = JComponents;
